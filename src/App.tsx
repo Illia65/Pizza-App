ce729b6c86097db9a9c84490a9d909bbbf5f23cb
Illia Ovhcaruk <ilaovcaruk0@gmail.com>
2024-01-04 19:36:39 +0000

    Connected Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Route, Routes } from "react-router-dom";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
 
 function App() {
   const [counter, setCounter] = useState<number>(0); //типипзация состояния
@@ -11,12 +15,38 @@ function App() {
 
   return (
     <>
-      <Button  appearance="small" onClick={addCounter}>Кнопка </Button>
-      <Button appearance="big" onClick={addCounter}>Кнопка </Button>
-      <Input isValid={true} placeholder="Email"/>
-   
+      <Button appearance="small" onClick={addCounter}>
+        Кнопка{" "}
+      </Button>
+      <Button appearance="big" onClick={addCounter}>
+        Кнопка{" "}
+      </Button>
+      <Input isValid={true} placeholder="Email" />
+
+      <div>
+        <a href="/menu">Меню</a>
+        <a href="/cart">Коризна</a>
+       
+      </div>
+      <Routes>
+        <Route path="menu" element={<Menu />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
 
 export default App;
+
+//у нас есть два роута на главной меню а на другой карт
+
+{
+  /* <Routes> 
+<Route path="/" element={<Menu/>}/>
+<Route path="Cart" element={<Cart/>}/>
+</Routes> */
+}
+
+//В новой версии для указания дефолтного
+//маршрута используется * вместо path="/".
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>    
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+ //подключение роутинга

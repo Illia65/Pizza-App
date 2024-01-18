c1d2bc78312df3ec3bbb923b9681409d45144eaa
Illia Ovhcaruk <ilaovcaruk0@gmail.com>
2024-01-18 16:47:44 +0000

    editing cart

--- src/components/Cartitem/CartItem.tsx
+++ src/components/Cartitem/CartItem.tsx
@@ -1,45 +1,46 @@
-// import styles from "./ProductCard.module.css";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store/store";
-import { cartActions } from "../../store/cart.slice";
-import styles from "./CartItem.module.css";
-import { CardItemProps } from "./CartItem.props";
+import styles from './CartItem.module.css';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { cartActions } from '../../store/cart.slice';
+import { CardItemProps } from './CartItem.props';
 
-const CardItem = (props: CardItemProps) => {
-  const dispatch = useDispatch<AppDispatch>();
+function CartItem(props: CardItemProps) {
+	const dispatch = useDispatch<AppDispatch>();
 
-  const increase = () => {
-    dispatch(cartActions.add(props.id));
-  };
+	const increase = () => {
+		dispatch(cartActions.add(props.id));
+	};
 
-  const descrease = () => {
-    dispatch(cartActions.add(props.id));
-  };
-  const remove = () => {
-    dispatch(cartActions.add(props.id));
-  };
+	const descrease = () => {
+		dispatch(cartActions.remove(props.id));
+	};
 
-  return (
-	<div className={styles['item']}>
-		<div className={styles['image']} style={{ backgroundImage: `url('${props.image}')` }}></div>
-		<div className={styles['description']}>
-			<div className={styles['name']}>{props.name}</div>
-			<div className={styles['price']}>{props.price}&nbsp;₽</div>
+	const remove = () => {
+		dispatch(cartActions.delete(props.id));
+	};
+
+
+	return (
+		<div className={styles['item']}>
+			<div className={styles['image']} style={{ backgroundImage: `url('${props.image}')` }}></div>
+			<div className={styles['description']}>
+				<div className={styles['name']}>{props.name}</div>
+				<div className={styles['price']}>{props.price}&nbsp;₽</div>
+			</div>
+			<div className={styles['actions']}>
+				<button className={styles['minus']} onClick={descrease}>
+					<img src="/minus-icon.svg" alt="Удалить из корзины" />
+				</button>
+				<div className={styles['number']}>{props.count}</div>
+				<button className={styles['plus']} onClick={increase}>
+					<img src="/plus-icon.svg" alt="Добавить в корзину" />
+				</button>
+				<button className={styles['remove']} onClick={remove}>
+					<img src="/delete-icon.svg" alt="Удалить все" />
+				</button>
+			</div>
 		</div>
-		<div className={styles['actions']}>
-			<button className={styles['minus']} onClick={descrease}>
-				<img src="/minus-icon.svg" alt="Удалить из корзины" />
-			</button>
-			<div className={styles['number']}>{props.count}</div>
-			<button className={styles['plus']} onClick={increase}>
-				<img src="/plus-icon.svg" alt="Добавить в корзину" />
-			</button>
-			<button className={styles['remove']} onClick={remove}>
-				<img src="/delete-icon.svg" alt="Удалить все" />
-			</button>
-		</div>
-	</div>
-);
-};
+	);
+}
 
-export default CardItem;
+export default CartItem;
--- src/store/cart.slice.ts
+++ src/store/cart.slice.ts
@@ -1,43 +1,46 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 export interface CartItem {
-    id: number
-    count: number
+    id: number;
+    count: number;
 }
 
 export interface CartState {
     items: CartItem[];
-
-
 }
 
 const initialState: CartState = {
     items: []
 };
 
-
-
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        add: (state, action: PayloadAction<number>) => {
-            const existed = state.items.find(i => i.id === action.payload);
-            if (!existed) {
-                state.items.push({ id: action.payload, count: 1 })
-                return;
-            }
-            state.items.map(i => {
-                if (i.id === action.payload) {
-                    i.count += 1
+        delete: (state, action: PayloadAction<number>) => {
+            state.items = state.items.filter(i => i.id !== action.payload);
+        },
+        remove: (state, action: PayloadAction<number>) => {
+            const existedIndex = state.items.findIndex(i => i.id === action.payload);
 
+            if (existedIndex !== -1) {
+                if (state.items[existedIndex].count === 1) {
+                    state.items = state.items.filter(i => i.id !== action.payload);
+                } else {
+                    state.items[existedIndex].count -= 1;
                 }
-                return i
-            })
+            }
+        },
+        add: (state, action: PayloadAction<number>) => {
+            const existedIndex = state.items.findIndex(i => i.id === action.payload);
+
+            if (existedIndex === -1) {
+                state.items.push({ id: action.payload, count: 1 });
+            } else {
+                state.items[existedIndex].count += 1;
+            }
         }
     }
-
-
 });
 
 export default cartSlice.reducer;

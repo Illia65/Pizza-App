500df257cc2f61d8ec159dc0ad744567bd120fd7
Illia Ovhcaruk <ilaovcaruk0@gmail.com>
2024-01-18 16:56:22 +0000

    saving (cart)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice, { JWT_PERSISTENT_STATE } from "./user.slice";
 import { saveState } from "./storage";
-import cartSlice from "./cart.slice";
+import cartSlice, { CART_PERSISTENT_STATE } from "./cart.slice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
-        cart:cartSlice
+        cart: cartSlice
 
     }
 });
 
 
 store.subscribe(() => {
-    saveState({jwt: store.getState().user.jwt }, JWT_PERSISTENT_STATE);
+    saveState({ jwt: store.getState().user.jwt }, JWT_PERSISTENT_STATE);
+    saveState(store.getState().cart, CART_PERSISTENT_STATE);
 })
 
 export type RootState = ReturnType<typeof store.getState>;
